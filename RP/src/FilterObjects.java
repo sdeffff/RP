462a3d50ae9e576a821794c24e1e34e7861ad769
Maksym <maxpavliy@gmail.com>
2024-11-16 22:08:07 +0100

    filter objs preparation

--- RP/src/FilterObjects.java
+++ RP/src/FilterObjects.java
@@ -1,5 +1,14 @@
+import javax.xml.parsers.*;
 import java.util.*;
+import java.io.*;
+import java.util.stream.*;
 
 public class FilterObjects {
+    private void filterObjsOnTheMap(String objName) {
+        try {
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
